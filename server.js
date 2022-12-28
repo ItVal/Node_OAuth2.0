75f4c60dfb2ce9bdb1073bae6204ac6d61fcff52
ItVal <valentinhonvj1@gmail.com>
2022-12-28 10:07:33 +0100

    config redirect endpoint

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require ('express');
 const port=4000;
 const app = express();
+
+const utils = require ('./utils');
+
+//redirect route
 app.get ('/auth', async (req, res) => {
-  res.send("auth route")
-});
+    try {
+      res.redirect (utils.request_get_auth_code_url);
+    } catch (error) {
+      res.sendStatus (500);
+      console.log (error.message);
+    }
+  });
+
 app.listen(port, () => console.log("server listening on port : " + port))
