f2160927163af5431cc27b728bf7083c08893d7e
ItVal <valentinhonvj1@gmail.com>
2022-12-28 10:19:49 +0100

    use autorization token to have access token

--- utils.js
+++ utils.js
@@ -1,6 +1,8 @@
 const query_string = require("querystring");
-const google_auth_token_endpoint =
-  "https://accounts.google.com/o/oauth2/v2/auth";
+const axios = require("axios")
+const google_auth_token_endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+
 const query_params = {
   client_id: process.env.CLIENT_ID,
   redirect_uri: `http://localhost:4000${process.env.REDIRECT_URI}`,
@@ -17,3 +19,17 @@ const request_get_auth_code_url = `${google_auth_token_endpoint}?${query_string.
   auth_token_params
 )}&scope=${scopes.join(" ")}`;
 module.exports = { request_get_auth_code_url };
+
+//use token access
+const get_access_token = async auth_code => {
+    const access_token_params = {
+      ...query_params,
+      client_secret: process.env.CLIENT_SECRET,
+      code: auth_code,
+      grant_type: 'authorization_code',
+    };
+    return await axios ({
+      method: 'post',
+      url: `${google_access_token_endpoint}?${query_string.stringify (access_token_params)}`,
+    });
+  };
